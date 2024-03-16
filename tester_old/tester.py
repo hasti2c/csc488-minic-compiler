be76db3d867effc962f96d70a56069d2313d40f2
Hasti Toossi <hasti.toossi@mail.utoronto.ca>
2024-03-16 19:00:58 -0400

    assignment 5 tester

--- tester_old/tester.py
+++ tester_old/tester.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 #
-# marker.py
+# tester.py
 #
-# Core tester framework for marker
+# Core tester framework for tester
 #
 
 import sys, os
 import argparse
 import subprocess
-import threading
-
+import time
 class Base:
     def __init__(self, message, total):
         parser = argparse.ArgumentParser(description='TestUnit')
-        parser.add_argument("path", type=str,
-                    help="path of source code")
         parser.add_argument('-v', '--verbose', action='store_true',
                             help='verbose mode')
         args = vars(parser.parse_args())
@@ -22,13 +19,10 @@ class Base:
             self.verbose = 1
         else:
             self.verbose = 0
-        self.path = args['path']
         self.message = message
         print(message)
-        self.public_mark = 0
-        self.private_mark = 0
         self.mark = 0
-        self.total = float(total)
+        self.total = total
         
     def __del__(self):
         if not hasattr(self, 'mark'):
@@ -39,29 +33,19 @@ class Base:
             print('mark = ' + str(self.mark) + ' is greater than ' \
                         'total = ' + str(self.total))
             print('\nPLEASE REPORT THIS TO THE INSTRUCTOR OR A TA\n')
-        print('Mark for ' + self.message + ' Public cases is ' + \
-              str(self.public_mark) + ' out of ' + str(self.total))
-        print('Mark for ' + self.message + ' Private cases is ' + \
-              str(self.private_mark) + ' out of ' + str(self.total))
         print('Mark for ' + self.message + ' is ' + \
               str(self.mark) + ' out of ' + str(self.total))
-        # marker = open('tester.out', 'a')
-        # marker.write(self.message + ', ' + str(self.mark) + \
-                    #  ', ' + str(self.total) + '\n')
-        # marker.close()
+        marker = open('tester.out', 'a')
+        marker.write(self.message + ', ' + str(self.mark) + \
+                     ', ' + str(self.total) + '\n')
+        marker.close()
         if self.mark == self.total:
             print('PASS')
         else:
             print('FAIL')        
 
     def add_mark(self, mark):
-        self.mark += float(mark)
-
-    def add_public_mark(self, mark):
-        self.public_mark += float(mark)
-        
-    def add_private_mark(self, mark):
-        self.private_mark += float(mark)
+        self.mark += int(mark)
 
     def reset_mark(self):
         self.mark = 0
@@ -84,7 +68,7 @@ class Core(Base):
             self.actual = exc.output      
         except Exception as e:
             print("ERROR: " + str(e))
-            #sys.exit(1)
+            sys.exit(1)
     
     def __init__(self, message, total):
         super().__init__(message=message, total=total)
@@ -92,20 +76,14 @@ class Core(Base):
     def __del__(self):
         super().__del__()
 
-    # Checks output against expected string
-    def check_output(self, expected, mark=1, isPublic=True):
+    def check_output(self, expected, mark=1):
         if self.verbose > 0:
             print('EXPECTING: ' + str(expected))
             print('FOUND: ' + str(self.actual))
         if self.actual == expected:
             self.add_mark(mark)
-            if isPublic:
-                self.add_public_mark(mark)
-            else:
-                self.add_private_mark(mark)
-            print('PASSED CASE')
         else:
-            print('FAILED CASE')
+            print('FAILED CASE: -{} mark(s)'.format(mark))
 
     def command_run(self, path, timeout=5):
         if self.verbose > 0:
@@ -114,29 +92,24 @@ class Core(Base):
             subprocess.call(path, timeout=timeout)
         except Exception as e:
             print("ERROR: " + str(e))
-            #sys.exit(1)
+            sys.exit(1)
 
     def generate_exec(self, paths):
         for path in paths:
             self.command_run(path)
 
     def exec_test(self, path, datain, getinput=False, timeout=5):
-        timer = None
         if self.verbose > 0:
             print('STARTING PROGRAM: ' + str(path))
         try:
             process = subprocess.Popen(path, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-            timer = threading.Timer(timeout, process.kill)
             if getinput:
                 process.stdin.write(datain)
-            timer.start()
             self.actual = process.communicate()[0]
             process.stdin.close()
         except Exception as e:
             print("ERROR: " + str(e))
-            #sys.exit(1)
-            if timer:
-                timer.cancel()
+            sys.exit(1)
 
     def clean_up (self, files):
         for f in files:
@@ -148,15 +121,16 @@ class Core(Base):
         line_count = len(nonempty_lines)
         file.close()
         return line_count
-
     def opt_check_output(self, f, optf, mark=1):
-        #line number check
-        l = self.line_number(f)
-        optl = self.line_number(optf)
-        if self.verbose > 0:
-            print('alloca count: line number: ' + str(l) + ' opt line number: ' + str(optl))
-        if l < optl:
-            print('FAILED CASE: -{} mark(s)'.format(mark))
+        #line number check. Comment it out if you want to check total line
+        # l = self.line_number(f)
+        # optl = self.line_number(optf)
+        # if self.verbose > 0:
+        #     print('alloca count: line number: ' + str(l) + ' opt line number: ' + str(optl))
+        # if l < optl:
+        #     print('FAILED CASE: -{} mark(s)'.format(mark))
+
+        #alloca instr removement check
         countsf = open(f, 'r').read().count("alloca")
         countsoptf = open(optf, 'r').read().count("alloca")
         if self.verbose > 0:
@@ -166,31 +140,19 @@ class Core(Base):
         else:
             print('FAILED CASE: -{} mark(s)'.format(mark))
 
-    def build_submission(self):
-        curr_dir = os.getcwd()
-        build_dir = str(curr_dir + '/build')
-        # Clean up local build directory
-        self.clean_up([build_dir])
-
-        # Try to build submission
-        self.command_run(['mkdir', build_dir])
-        self.command_run(['cmake', (self.path), '-B', build_dir], timeout=30)
-        self.command_run(['make', '-C', build_dir], timeout=200)
-        self.exec_path = build_dir + '/src/minicc'
-        self.build_dir = build_dir
-
-    def init_wo_build(self):
-        self.exec_path = self.path + '/build/src/minicc'
-        self.build_dir = self.path + '/build'
-        
-    def command_run_with_marks(self, paths, mark=1, timeout=5):
-        for path in paths:
-            if self.verbose > 0:
-                print('RUNNING COMMAND: ' + str(path))
-            try:            
-                subprocess.check_output(path, timeout=timeout)
-            except subprocess.CalledProcessError as exc:
-                print('FAILED CASE: -{} mark(s)'.format(mark))
-                self.add_mark(-mark)
-                break
-        self.add_mark(mark)
+    def exec_timetest(self, iter, path, datain, getinput=False):
+        print('Time Test:' + str(path))
+        start = time.time()
+        for i in range(iter):
+            try:
+                process = subprocess.Popen(path, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                if getinput:
+                    process.stdin.write(datain)
+                _ = process.communicate()[0]
+                process.stdin.close()
+            except Exception as e:
+                print("ERROR: " + str(e))
+                sys.exit(1)
+        end = time.time()
+        print("Time Result:", end - start, "seconds")
+       
