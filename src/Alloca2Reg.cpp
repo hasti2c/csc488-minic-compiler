9e5f8315269532fc82bd3cec9dfc7d31ba308f04
Hasti Toossi <hasti.toossi@mail.utoronto.ca>
2024-04-06 13:50:23 -0400

    debugged A6

--- src/Alloca2Reg.cpp
+++ src/Alloca2Reg.cpp
@@ -10,9 +10,9 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/IR/CFG.h"
+#include "llvm/IR/Constants.h"
 #include <set>
 #include <map>
-#include <iostream> // TODO
 
 using namespace llvm;
 
@@ -25,16 +25,14 @@ namespace {
         std::set<AllocaInst*> TargetAllocas;
         std::map<BasicBlock*, std::map<AllocaInst*, Value*> > Post;
         std::map<BasicBlock*, std::map<AllocaInst*, PHINode*> > Pre;
-        std::map<LoadInst*, PHINode*> loadPHIs;
+        std::map<BasicBlock*, std::map<LoadInst*, PHINode*> > loads;
+        std::map<BasicBlock*, std::set<std::pair<AllocaInst*, PHINode*> > > PHIs;
 
-        std::set<std::pair<AllocaInst*, PHINode*> > getPHINodes(BasicBlock* bb) { // TODO: global var
-            std::set<std::pair<AllocaInst*, PHINode*> > phis;
-            for (auto it = Pre[bb].begin(); it != Pre[bb].end(); it++)
-                phis.insert(*it);
-            for (auto it = Post[bb].begin(); it != Post[bb].end(); it++)
-                if (PHINode *phi = dyn_cast<PHINode>(it->second))
-                    phis.insert(std::pair<AllocaInst*, PHINode*>(it->first, phi));
-            return phis;
+        Value *createPHIOrUndef(BasicBlock *bb, Type *type) {
+            if (pred_size(bb))
+                return PHINode::Create(type, pred_size(bb));
+            else
+                return UndefValue::get(type);
         }
 
         void collectTargetAllocas(Function &F) {
@@ -62,18 +60,26 @@ namespace {
                             if (Post[bb].find(alloca) != Post[bb].end())
                                 load->replaceAllUsesWith(Post[bb][alloca]);
                             else {
-                                PHINode *phi = PHINode::Create(load->getType(), pred_size(bb));
-                                Pre[bb][alloca] = phi;
-                                Post[bb][alloca] = phi;
-                                loadPHIs[load] = phi;
+                                Value *val = createPHIOrUndef(bb, load->getType());
+                                Post[bb][alloca] = val;
+                                if (PHINode *phi = dyn_cast<PHINode>(val)) {
+                                    Pre[bb][alloca] = phi;
+                                    loads[bb][load] = phi;
+                                    PHIs[bb].insert(std::pair<AllocaInst*, PHINode*>(alloca, phi));
+                                } else
+                                    load->replaceAllUsesWith(val);
                             }
                         }
                     }
                 }
                 for (auto it = TargetAllocas.begin(); it != TargetAllocas.end(); it++) {
                     AllocaInst *alloca = *it;
-                    if (Post[bb].find(alloca) == Post[bb].end())
-                        Post[bb][alloca] = PHINode::Create(alloca->getAllocatedType(), pred_size(bb));
+                    if (Post[bb].find(alloca) == Post[bb].end()) {
+                        Value *val = createPHIOrUndef(bb, alloca->getAllocatedType());
+                        Post[bb][alloca] = val;
+                        if (PHINode *phi = dyn_cast<PHINode>(val)) 
+                            PHIs[bb].insert(std::pair<AllocaInst*, PHINode*>(alloca, phi));
+                    }
                 }
             }
         }
@@ -81,8 +87,7 @@ namespace {
         void fillPHIs(Function &F) {
             for (Function::iterator bb_it = F.begin(); bb_it != F.end(); bb_it++) {
                 BasicBlock *bb = (BasicBlock*) &*bb_it;
-                std::set<std::pair<AllocaInst*, PHINode*> > phis = getPHINodes(bb);
-                for (auto phi_it = phis.begin(); phi_it != phis.end(); phi_it++) {
+                for (auto phi_it = PHIs[bb].begin(); phi_it != PHIs[bb].end(); phi_it++) {
                     AllocaInst *alloca = phi_it->first;
                     PHINode *phi = phi_it->second;
                     for (auto pred_it = predecessors(bb).begin(); pred_it != predecessors(bb).end(); pred_it++) {
@@ -97,30 +102,33 @@ namespace {
             for (Function::iterator bb_it = F.begin(); bb_it != F.end(); bb_it++) {
                 BasicBlock *bb = (BasicBlock*) &*bb_it;
                 Instruction *begin = (Instruction*) &*(bb->begin());
-                std::set<std::pair<AllocaInst*, PHINode*> > phis = getPHINodes(bb);
-                for (auto phi_it = phis.begin(); phi_it != phis.end(); phi_it++) {
+                for (auto phi_it = PHIs[bb].begin(); phi_it != PHIs[bb].end(); phi_it++) {
                     AllocaInst *alloca = phi_it->first;
                     PHINode *phi = phi_it->second;
                     phi->insertBefore(begin);
                 }
-            }
-            for (auto it = loadPHIs.begin(); it != loadPHIs.end(); it++) {
-                LoadInst *load = it->first;
-                PHINode *phi = it->second;
-                load->replaceAllUsesWith(phi);
+                for (auto it = loads[bb].begin(); it != loads[bb].end(); it++) {
+                    LoadInst *load = it->first;
+                    PHINode *phi = it->second;
+                    load->replaceAllUsesWith(phi);
+                }
             }
         }
 
         void cleanUnused(Function &F) {
             for (Function::iterator bb_it = F.begin(); bb_it != F.end(); bb_it++) {
                 BasicBlock *bb = (BasicBlock*) &*bb_it;
-                std::set<std::pair<AllocaInst*, PHINode*> > phis = getPHINodes(bb);
-                for (auto phi_it = phis.begin(); phi_it != phis.end(); phi_it++) {
+                std::set<std::pair<AllocaInst*, PHINode*> > removed;
+                for (auto phi_it = PHIs[bb].begin(); phi_it != PHIs[bb].end(); phi_it++) {
                     AllocaInst *alloca = phi_it->first;
                     PHINode *phi = phi_it->second;
-                    if (phi->use_empty())
+                    if (phi->use_empty()) {
                         phi->eraseFromParent();
+                        removed.insert(*phi_it);
+                    }
                 }
+                for (auto it = removed.begin(); it != removed.end(); it++)
+                    PHIs[bb].erase(*it);
             }
             for (Function::iterator bb_it = F.begin(); bb_it != F.end(); bb_it++) {
                 BasicBlock *bb = (BasicBlock*) &*bb_it;
@@ -150,18 +158,34 @@ namespace {
                 (*it)->eraseFromParent();
         }
 
+        void cleanRedundant(Function &F) {
+            for (Function::iterator bb_it = F.begin(); bb_it != F.end(); bb_it++) {
+                BasicBlock *bb = (BasicBlock*) &*bb_it;
+                for (auto phi_it = PHIs[bb].begin(); phi_it != PHIs[bb].end(); phi_it++) {
+                    AllocaInst *alloca = phi_it->first;
+                    PHINode *phi = phi_it->second;
+                    Value *val = phi->hasConstantValue();
+                    if (val != nullptr) {
+                        phi->replaceAllUsesWith(val);
+                        phi->eraseFromParent();
+                    }
+                }
+            }
+        }
+
         virtual bool runOnFunction(Function &F) {
             errs() << "Working on function called " << F.getName() << "!\n";
             TargetAllocas.clear();
             Post.clear();
             Pre.clear();
-            loadPHIs.clear();
+            loads.clear();
+            PHIs.clear();
             collectTargetAllocas(F);
-            //start your code here
             collectPosts(F);
             fillPHIs(F);
             insertPHIs(F);
             cleanUnused(F);
+            cleanRedundant(F);
             return true;
         }
     };
